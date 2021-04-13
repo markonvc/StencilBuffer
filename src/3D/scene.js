1adecbe26ccbdd533db1d745d921acdd4ad30c37
Marko Novic <marko.novic@marbleit.rs>
2021-04-13 17:15:53 +0200

    Working with two scenes

--- src/3D/scene.js
+++ src/3D/scene.js
@@ -12,8 +12,21 @@ import {OutlineEffect} from 'three/examples/jsm/effects/OutlineEffect'
 
 const Feature = () => { 
     const canvas = document.getElementById("canvas");
+    let raycasterOne = new THREE.Raycaster()
+    // let raycasterTwo = new THREE.Raycaster()
+    const mouse = new THREE.Vector2();
+
+    function onMouseMove( event ) {
+
+      // calculate mouse position in normalized device coordinates
+      // (-1 to +1) for both components
+      raycasterOne.setFromCamera( mouse, camera );
+      mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+      mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    }  
 
     let scene = new THREE.Scene();
+    let sceneTwo = new THREE.Scene();
 
     let camera = new THREE.PerspectiveCamera(
         100,
@@ -56,26 +69,45 @@ const Feature = () => {
 				controls.dampingFactor = 0.05;
 
 
-    const loader =  new FBXLoader();
-    const path = "https://configurator.mag.archi/media/gnunm1ll/kubusi-2-1.fbx";
+    // const loader =  new FBXLoader();
+    // const path = "https://configurator.mag.archi/media/gnunm1ll/kubusi-2-1.fbx";
 
 
     var color = new THREE.Color(255, 0, 0);
-    var mat = new THREE.MeshBasicMaterial({ color: color, opacity: 0.3, transparent: true});
+    var mat = new THREE.MeshBasicMaterial({ color: color, opacity: 0, transparent: true});
     let geometry3 = new THREE.BoxGeometry( 2, 2, 2 );
     var mesh = new THREE.Mesh(geometry3, mat); 
     // mesh.material. = 0; 
     console.log(mesh)
+    mesh.name = "01"
     scene.add(mesh);
 
     var color2 = new THREE.Color(0, 255, 255);
-    var mat2 = new THREE.MeshBasicMaterial({ color: color2});
+    var mat2 = new THREE.MeshBasicMaterial({ color: color2, opacity: 0, transparent: true});
     let geometry2 = new THREE.BoxGeometry( 2, 2, 2 );
     var mesh2 = new THREE.Mesh(geometry2, mat2); 
-    mesh2.position.x = 0.5;
-    mesh2.position.z = -3;
-    mesh2.position.y = 0
-    scene.add(mesh2);
+    mesh2.position.x = 3;
+    mesh2.position.z = 0;
+    mesh2.position.y = 0;
+    mesh2.name = "02"
+    console.log(mesh2);
+    sceneTwo.add(mesh2);
+
+    window.addEventListener( 'mousemove', onMouseMove, false );
+
+    // let intersectsSceneOne = [];
+    //   for(let child in scene.children) {
+    //   intersectsSceneOne = raycasterOne.intersectObjects(
+    //     scene.children[child].children);
+    //     console.log(intersectsSceneOne);
+    // }
+
+
+    // let intersectsSceneTwo = [];
+    // for(let child in sceneTwo.children) {
+    //   intersectsSceneTwo = raycasterOne.intersectObjects(
+    //     sceneTwo.children[child].children);
+    // }
 
 
     // loader.load(path, (model) => {
@@ -103,12 +135,25 @@ const Feature = () => {
     // scene.add(light);
 
     var render = function(){
-        
-     
-        requestAnimationFrame(render);
-        renderer.render(scene, camera);
-    }
+        raycasterOne.setFromCamera( mouse, camera );
+        const intersects = raycasterOne.intersectObjects( scene.children );
 
+        for ( let i = 0; i < intersects.length; i ++ ) {
+          intersects[ 0 ].object.material.opacity = 1;
+          console.log(intersects[i])
+          if(intersects[i].name !== intersects[0].name) {
+            intersects[i].material.opacity = 0
+          }
+        }
+      
+        renderer.clear()
+        renderer.render(scene, camera);
+        console.log("two");
+        // renderer.clearDepth();
+        // renderer.render(sceneTwo, camera);
+        requestAnimationFrame(render);
+    }
+  
     render();
 
 }
